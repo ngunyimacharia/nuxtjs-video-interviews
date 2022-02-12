e37911969ca0bfd77613bd3b0202f4749ac8bc93
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2022-02-12 17:41:06 +0200

    Submit form added

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="m-20">
-    <h1 class="my-10 text-3xl text-center font-bold text-gray-900">
+    <h1 class="text-3xl text-center font-bold text-gray-900">
       Submit your interview
     </h1>
+    <h2 class="my-5 text-center leading-6 text-gray-600">For best experience, use either Firefox or Chome</h2>
     <div class="my-5" v-for="(question,index) in questions" :key="index">
-      <h2 class=" leading-6 text-gray-600">{{index+1}}. {{question.question}}</h2>
+      <h3 class=" leading-6 text-gray-600">{{index+1}}. {{question.question}}</h3>
       <div class="mx-10">
         <video v-if="!question.recording && question.answer" class="w-96 h-56 my-5 bg-gray-700" :src="question.answer" controls></video>
         <video v-else-if="question.recording"  class="w-96 h-56 my-5 bg-gray-700" :id="`player-${index}`" controls="false"></video>
@@ -16,10 +17,18 @@
         </button>
       </div>
     </div>
-    <div class="my-5 text-right">
-      <button type="button" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-        Submit interview
-      </button>
+    <div class="my-5 text-right flex flex-row-reverse">
+      
+      <form class="w-1/3" @submit.prevent="submit">
+        <div class="mt-1 flex border border-gray-300  rounded-md shadow-sm">
+          <div class="relative flex items-stretch flex-grow focus-within:z-10">
+            <input required v-model="interviewee" type="text" class="focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:text-sm border-gray-300" placeholder="Enter your name...">
+          </div>
+          <button type="submit" class="-ml-px relative inline-flex items-center space-x-2 px-4 py-2 border border-indigo-600 text-sm font-medium rounded-r-md  text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none">
+            Submit interview
+          </button>
+        </div>
+      </form>
     </div>
   </div>
 
@@ -30,6 +39,7 @@
 export default {
   data(){
     return {
+      interviewee:null,
       questions:[
         {
           question: "Tell me something about yourself.",
@@ -90,6 +100,9 @@ export default {
         document.getElementById(`player-${index}`).srcObject = null;
         this.questions[index].answer = URL.createObjectURL(new Blob(this.questions[index].recordedChunks), {type: 'video/mp4'});
     },
+    submit(){
+      alert("Submitting");
+    }
   }
 }
 </script>
